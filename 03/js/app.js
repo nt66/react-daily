ad0ba1796a62b88e5a0c84b325f5af19d5fec879
hualingfeng <tc_hualingfeng@163.com>
2017-01-07 23:30:10 +0800

    mixins

--- 03/js/app.js
+++ 03/js/app.js
@@ -1,28 +1,28 @@
 /**
- * events方式传递消息
+ * mixins属性示例
+ * 只支持creareClass
  * 华凌锋
  */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import emitter from './events';
-import List from './List.js';
+import MixinComponent from './MixinComponent.js';
 
-export default class App extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    componentDidMount() {
-        this.itemClick = emitter.addListener('ItemClick', (msg, data) => {
-            console.log(msg, data);
-        });
-    }
-    componentWillUnmount() {
-        emitter.removeListener(this.itemClick);
-    }
+var App = React.createClass({
+    getInitialState(){
+        return{
+            // text:'asdasd' //不同设置相同的state
+        }
+    },
+    mixins: [MixinComponent],
+    componentWillMount(){
+        console.log('app will mount');
+    },
+    //不能设置相同的方法
+    // func(){
+    //   return '我来自app';  
+    // },
     render() {
-        return (
-            <List />
-        )
+        return (<span>{this.func()}</span>)
     }
-}
+});
 ReactDOM.render(<App />, document.getElementById('root'));
